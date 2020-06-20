1498607e6faf57493fa2563c7d71bb8244a959b3
kuvarov <s>
2020-06-20 17:56:27 +0300

    add comment

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+
+        while (x < 5) {
+            // y = x - y;
+            // y = y + x;
+            y = y + 2;
+            if (y > 4) {
+                y--;
+            }
+            System.out.print(x + " " + y + " ");
+            x++;
+        }
+    }
+}
+

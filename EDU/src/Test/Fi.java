f2639b041dd4baef538162c8f6411d74ba4512cc
kuvarov <uvarovkirill.v@gmail.com>
2022-04-01 14:46:55 +0300

    comment in master

--- EDU/src/Test/Fi.java
+++ EDU/src/Test/Fi.java
@@ -1,7 +1,7 @@
 package Test;
 
 public class Fi {
-    // comment
+    // comment in test branch
     public static void main(String[] args) {
         int n = 1000;
         long[] f = new long[n];

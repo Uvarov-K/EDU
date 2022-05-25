a4332665a86923d1c4508c90f9e91573c4942d26
kuvarov <uvarovkirill.v@gmail.com>
2022-05-25 19:26:12 +0300

    corrected withdraw

--- EDU/src/Test/Terminal.java
+++ EDU/src/Test/Terminal.java
@@ -2,6 +2,8 @@ package Test;
 
 import java.util.Scanner;
 
+import static java.awt.SystemColor.menu;
+
 public class Terminal {
     double balance = 100;
 
@@ -12,7 +14,7 @@ public class Terminal {
         System.out.println("Insert your money ");
         double d = insertMoney.nextDouble();
         System.out.println("You inserted: " + d + "\n" +
-                "Do you want to continue? [Y/y N/n]");
+                           "Do you want to continue? [Y/y N/n]");
 
         double sum = d;
         while (!answer.next().equals("n")) {
@@ -21,7 +23,7 @@ public class Terminal {
             d = insertMoney.nextDouble();
             sum = sum + d;
             System.out.println("You inserted: " + sum + "\n" +
-                    "Do you want to continue? [Y/y N/n]");
+                               "Do you want to continue? [Y/y N/n]");
         }
         balance = sum + balance;
         System.out.println("Your balance is: " + balance + "\n");
@@ -30,27 +32,26 @@ public class Terminal {
 
 
     private void withdrawMoney() {
-        Scanner withdrawMoney = new Scanner(System.in);
+        Scanner withdrawAmount = new Scanner(System.in);
         Scanner answer = new Scanner(System.in);
 
-        System.out.println("Input amount ");
-        double d = withdrawMoney.nextDouble();
-        System.out.println("You inputted: " + d + "\n" +
-                "Do you want to continue? [Y/y N/n]");
+        System.out.println("Available: " + balance + "\n" + "Input amount ");
+        double d = withdrawAmount.nextDouble();
 
-        double sum = d;
-        while (!answer.next().equals("n")) {
-            System.out.println("Input amount");
-
-            d = withdrawMoney.nextDouble();
-            sum = sum + d;
-            System.out.println("You inputted: " + sum + "\n" +
-                    "Do you want to continue? [Y/y N/n]");
+        while (d > balance) {
+            System.out.println("You can't withdraw more than " + balance);
+            d = withdrawAmount.nextDouble();
         }
-        balance = balance - sum;
-        System.out.println("Your balance is: " + balance + "\n");
+        System.out.println("You entered: " + d + "\n" + "Do you want to proceed? [Y/y N/n]");
+        while (!answer.next().equals("y")) {
+            System.out.println("Incorrect symbol, type 'Y' or 'N'");
+            if (answer.next().equals("n")) {
+                menu();
+            }
+        }
+        System.out.println("exit");
         menu();
-    }
+   }
 
 
     private void showBalance() {
@@ -61,26 +62,32 @@ public class Terminal {
 
     private void menu() {
         System.out.println(
-                """
-                         ====================\s
-                         Available actions:\s
-                         1) insert money\s
-                         2) withdraw money\s
-                         3) show balance\s
-                         4) exit
-                         ====================\s
-                        """
+                " ==================== \n" +
+                " Available actions: \n" +
+                " 1) insert money \n" +
+                " 2) withdraw money \n" +
+                " 3) show balance \n" +
+                " 4) exit\n" +
+                " ==================== \n"
         );
 
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
 
         switch (i) {
-            case 1: insertMoney(); break;
-            case 2: withdrawMoney(); break;
-            case 3: showBalance(); break;
-            case 4: break;
-            default: break;
+            case 1:
+                insertMoney();
+                break;
+            case 2:
+                withdrawMoney();
+                break;
+            case 3:
+                showBalance();
+                break;
+            case 4:
+                break;
+            default:
+                break;
         }
     }
 

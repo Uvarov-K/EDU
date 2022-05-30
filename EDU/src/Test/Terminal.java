a2dcebe89a1c5dd9955fd9db305d7ff9378fdff8
kuvarov <uvarovkirill.v@gmail.com>
2022-05-30 18:08:03 +0300

    corrected withdraw

--- EDU/src/Test/Terminal.java
+++ EDU/src/Test/Terminal.java
@@ -1,5 +1,6 @@
 package Test;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Terminal {
@@ -33,34 +34,51 @@ public class Terminal {
     private void withdrawMoney() {
         Scanner withdrawAmount = new Scanner(System.in);
         Scanner answer = new Scanner(System.in);
+        boolean isTrue = false;
 
         System.out.print("Available: " + balance + "\n" + "Input amount: ");
-        double d = withdrawAmount.nextDouble();
 
-        while (d > balance || d < 0) {
-            System.out.println("You can't withdraw more than " + balance);
-            d = withdrawAmount.nextDouble();
+        while (!isTrue) {
+            try {
+                double d = withdrawAmount.nextDouble();
+                while (d > balance || d < 0) {
+                    if (d > balance) {
+                        System.out.println("You can't withdraw more than " + balance);
+                    } else if (d < 0) {
+                        System.out.println("Don't try to fuck me! ");
+                    }
+                    d = withdrawAmount.nextDouble();
+                }
+
+                System.out.print("You entered: " + d + "\n" + "Do you want to proceed? [Y/y N/n]: ");
+
+                String s = answer.nextLine();
+                while (!s.equals("y") && !s.equals("n")) {
+                    System.out.print("Incorrect symbol, type 'Y' or 'N'");
+                    s = answer.nextLine();
+                }
+                if (s.equals("n")) {
+                    System.out.println("Returning to main menu");
+                    isTrue = true;
+                    menu();
+                } else {
+                    System.out.println("*** sounds of cash withdrawal *** ");
+                    balance = balance - d;
+                    System.out.println("Your balance is: " + balance);
+                    isTrue = true;
+                    menu();
+                }
+
+            } catch (InputMismatchException e) {
+                withdrawAmount.next();
+                System.out.println("Please enter a number! ");
+
+            }
+
         }
 
 
-        System.out.print("You entered: " + d + "\n" + "Do you want to proceed? [Y/y N/n]: ");
-
-        String s = answer.nextLine();
-        while (!s.equals("y") && !s.equals("n")) {
-            System.out.print("Incorrect symbol, type 'Y' or 'N'");
-            s = answer.nextLine();
-        }
-        if (s.equals("n")) {
-            System.out.println("Returning to main menu");
-            menu();
-        } else {
-            System.out.println("*** sounds of cash withdrawal *** ");
-            balance = balance - d;
-            System.out.println("Your balance is: " + balance);
-            menu();
-        }
     }
-
 
     private void showBalance() {
         System.out.println("Your balance is: " + balance);

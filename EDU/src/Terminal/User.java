acbec2b545e3b1c078e9a32a3e9868d4f333922b
kuvarov <uvarovkirill.v@gmail.con>
2022-07-14 20:48:46 +0300

    refactoring

--- EDU/src/Terminal/User.java
+++ EDU/src/Terminal/User.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class User {
     static Scanner scanner  = new Scanner(System.in);
     static int userID = scanner.nextInt();
-    //static int userPIN = scanner.nextInt();
+
 
 
 }
